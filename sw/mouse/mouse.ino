6f56e560bfe36b96d8fcefd8f1c9dd443557a815
Tim <tim@congoblue.me>
2026-01-22 17:11:35 +0000

    sw changes

--- sw/mouse/mouse.ino
+++ sw/mouse/mouse.ino
@@ -2,15 +2,22 @@
 
 #include <math.h> 
 
-uint8_t pinstate=1, KeyHit=0, LastKey, seq=0, faderactive=0;  
+uint8_t pinstate=1, KeyHit=0, LastKey, seq=0, faderactive0=0, faderactive1=0;  
 
-uint8_t enca, encb, encc;
+int32_t Enca, Encb, Encc;
+int32_t timea=0,timeb=0,timec=0;
+uint8_t EncaChange, EncbChange, EnccChange;
+uint8_t EncaNew, EncbNew, EnccNew;
 
-int preva0;
+int preva0, preva1, encoffset;
 
-uint32_t atime=0,v,fp;
+uint32_t atime0=0,atime1=0,v,fp;
 
 uint32_t KeyState=0, kr=0;
+
+uint8_t mousedn=0, setupmode=0;
+
+uint32_t xp[32],yp[32];
 
 
 #define AN1 A6
@@ -32,11 +39,14 @@ uint32_t KeyState=0, kr=0;
 #define ROT1B D11
 #define ROT2A D12
 #define ROT2B D13
+
+#define ENCODER_TIMEOUT 500
+#define FADER_TIMEOUT 250  
   
 
 void setup() { 
 
-  //AbsMouse.init(1920,1080); 
+  AbsMouse.init(1920,1080); 
 
   Serial.begin(115200);
 
@@ -49,12 +59,12 @@ void setup() {
   pinMode(SW7, INPUT_PULLUP);
   pinMode(SW8, INPUT_PULLUP);
 
-  pinMode(ROT1A, INPUT);
-  pinMode(ROT1B, INPUT);
-  pinMode(ROT2A, INPUT);
-  pinMode(ROT2B, INPUT);
-  pinMode(ROT3A, INPUT);
-  pinMode(ROT3B, INPUT);
+  pinMode(ROT1A, INPUT_PULLUP);
+  pinMode(ROT1B, INPUT_PULLUP);
+  pinMode(ROT2A, INPUT_PULLUP);
+  pinMode(ROT2B, INPUT_PULLUP);
+  pinMode(ROT3A, INPUT_PULLUP);
+  pinMode(ROT3B, INPUT_PULLUP);
 
   digitalWrite(SWCOM1,1);
   digitalWrite(SWCOM2,1);
@@ -69,65 +79,58 @@ void setup() {
   pinMode(AN0, INPUT); //fader 1
   pinMode(AN1, INPUT); //fader 2
 
+  xp[24]=1627;
+  yp[24]=859;
+
   delay(1000); // Let USB settle 
 
   Serial.print("Avoremote\r\n");
 
 } 
 
-void EncoderISR(void)
+void EncoderPoll(void)
 {
-  static char prevenc=0x0F;
-  char nowenc=(PIND>>2)&0x0F;
+  static uint8_t prevenc=0x00;
+  int8_t enca=0, encb=0, encc=0;
+  uint8_t nowenc=digitalRead(ROT1A)|(digitalRead(ROT1B)<<1)|(digitalRead(ROT2A)<<2)|(digitalRead(ROT2B)<<3)|(digitalRead(ROT3A)<<4)|(digitalRead(ROT3B)<<5);
+
   char diff=prevenc^nowenc;
 
-  if (diff&0x01)
+  if (diff&1)
   {
-    if (digitalRead(ROT1B)==0)
-    {    
-      if (digitalRead(ROT1A)==0)enca++; else enca--;
+    if (prevenc&1)  //falling edge only
+    {
+        
+        if (nowenc&2) enca=+1; else enca=-1;
+        if (timea!=0) {Enca=(millis()-timea)*enca; EncaChange=1;} else EncaNew=1;
+        timea=millis();
     }
-    else
-    {    
-      if (digitalRead(ROT1A)==0)enca--; else enca++;
-    }    
   }
-  if (diff&0x02)
-  {   
-    if (digitalRead(ROT1A)==0)
-    {    
-      if (digitalRead(ROT1B)==0)enca--; else enca++;
-    }
-    else
-    {    
-      if (digitalRead(ROT1B)==0)enca++; else enca--;
-    }    
-  }
-  
-  
-  if (diff&0x04)
+
+  if (diff&4)
   {
-    if (digitalRead(ENC2B)==0)
-    {    
-      if (digitalRead(ENC2A)==0)encb++; else encb--;
+    if (prevenc&4) 
+    {
+        
+        if (nowenc&8) encb=+1; else encb=-1;
+        if (timeb!=0) {Encb=(millis()-timeb)*encb; EncbChange=1;} else EncbNew=1;
+        timeb=millis();
     }
-    else
-    {    
-      if (digitalRead(ENC2A)==0)encb--; else encb++;
-    }    
   }
-  if (diff&0x08)
-  {   
-    if (digitalRead(ENC2A)==0)
-    {    
-      if (digitalRead(ENC2B)==0)encb--; else encb++;
+
+  if (diff&16)
+  {
+    if (prevenc&16) 
+    {
+        
+        if (nowenc&32) encc=+1; else encc=-1;
+        if (timec!=0) {Encc=(millis()-timec)*encc; EnccChange=1;} else EnccNew=1;
+        timec=millis();
     }
-    else
-    {    
-      if (digitalRead(ENC2B)==0)encb++; else encb--;
-    }    
   }
-  prevenc=(PIND>>2)&0x0F;
+
+
+  prevenc=nowenc;
 }
 
 uint8_t SwRead(void)  
@@ -146,9 +149,105 @@ uint8_t SwRead(void)
 
 void loop() { 
 
-  uint32_t a0,v,i;
+  static uint8_t x=0;
+  uint32_t a0,a1,v,i;
   static uint8_t ld=0;
   static uint8_t ks=0;
+
+  x++;
+
+  EncoderPoll();
+
+  if (EncaChange)
+  {
+    EncaChange=0;
+    if (EncaNew)
+    {
+      EncaNew=0;
+      if (mousedn!=0) {AbsMouse.release(MOUSE_LEFT); faderactive0=0; faderactive1=0; timeb=0; timec=0;}
+      AbsMouse.move(xp[24],yp[24]); 
+      AbsMouse.press(MOUSE_LEFT);
+      encoffset=0;
+      mousedn=1;
+    }
+    else
+    {
+      Serial.println(Enca);
+      if (abs(Enca)>=50) v=1; else v=1+(50-abs(Enca))/2;
+      if (Enca>0) 
+      {
+        encoffset+=v; 
+      }
+      else 
+      {
+        if (encoffset+xp[24]>v) encoffset-=v; else encoffset=xp[24];
+      }      
+      AbsMouse.move(xp[24]+encoffset,yp[24]); 
+    }
+
+  }
+
+  if (EncbChange)
+  {
+    EncbChange=0;
+    if (EncbNew)
+    {
+      EncbNew=0;
+      if (mousedn!=0) {AbsMouse.release(MOUSE_LEFT); faderactive0=0; faderactive1=0; timea=0; timec=0;}
+      AbsMouse.move(xp[25],yp[25]); 
+      AbsMouse.press(MOUSE_LEFT);
+      encoffset=0;
+      mousedn=1;
+    }
+    else
+    {
+      if (Encb>0) encoffset++; else encoffset--;
+      AbsMouse.move(xp[25]+encoffset,yp[25]); 
+    }
+  }
+
+  if (EnccChange)
+  {
+    EnccChange=0;
+    if (EnccNew)
+    {
+      EnccNew=0;
+      if (mousedn!=0) {AbsMouse.release(MOUSE_LEFT); faderactive0=0; faderactive1=0; timea=0; timeb=0;}
+      AbsMouse.move(xp[26],yp[26]); 
+      AbsMouse.press(MOUSE_LEFT);
+      encoffset=0;
+      mousedn=1;
+    }
+    else
+    {
+      if (Encc>0) encoffset++; else encoffset--;
+      AbsMouse.move(xp[26]+encoffset,yp[26]); 
+    }
+  }
+
+
+  if (timea!=0) if ((millis()-timea)>ENCODER_TIMEOUT) 
+  {
+    timea=0;
+    AbsMouse.release(MOUSE_LEFT);
+    mousedn=0;
+  }
+  if (timeb!=0) if ((millis()-timeb)>ENCODER_TIMEOUT) 
+  {
+    timeb=0;
+    AbsMouse.release(MOUSE_LEFT);
+    mousedn=0;
+  }
+  if (timec!=0) if ((millis()-timec)>ENCODER_TIMEOUT) 
+  {
+    timec=0;
+    AbsMouse.release(MOUSE_LEFT);
+    mousedn=0;
+  }
+
+  if (x>=10)
+  {
+    x=0;
 
   //key scan
   if (ks==0) 
@@ -190,6 +289,7 @@ void loop() {
     }
   }
   
+  //diagnostic
 
    if (KeyHit!=0)
    {
@@ -203,50 +303,73 @@ void loop() {
    {
       ld=0; 
       digitalWrite(LED_BUILTIN,0); 
+      //Serial.println(nowenc,HEX);
    }
 
    if (KeyHit!=0)
    {
-      /*seq++; if (seq>3) seq=0;
-      if (seq==0) AbsMouse.move(10, 10); 
-      if (seq==1) AbsMouse.move(1900, 10); 
-      if (seq==2) AbsMouse.move(1900, 1000); 
-      if (seq==3) AbsMouse.move(10, 1000); */
-      if (KeyHit==1) AbsMouse.move(1498, 1012); //go
-      if (KeyHit==2) AbsMouse.move(1461, 961);  //cue
-      if (KeyHit==4) AbsMouse.move(1391, 854);  //back
-      AbsMouse.press(MOUSE_LEFT);
       KeyHit=0;
+      AbsMouse.move(xp[LastKey],yp[LastKey]); 
+      AbsMouse.press(MOUSE_LEFT);
       delay(100); 
       AbsMouse.release(MOUSE_LEFT);
-      faderactive=0;
+      faderactive0=0;
       delay(100); 
    }
 
-   a0=analogRead(A0);
+   a0=analogRead(AN0);
    if (abs(a0-preva0)>20)
    {
+      //Serial.println(a0);
       preva0=a0;
-      if (faderactive==0) fp=a0; //this is a new move starting. Remember where we started off from
-      atime=millis();
+      if (faderactive0==0) fp=a0; //this is a new move starting. Remember where we started off from
+      atime0=millis();
       v=932+((1022-966)*2*(fp-a0)/1024);
       AbsMouse.move(1361, v);
-      if (faderactive==0) 
+      if (faderactive0==0) 
       {
-            faderactive=1;
+            faderactive0=1;
+            if (mousedn!=0) {AbsMouse.release(MOUSE_LEFT); faderactive1=0; timea=0; timeb=0; timec=0;}
             AbsMouse.press(MOUSE_LEFT);
+            mousedn=1;
       }
    }
 
-   if ((faderactive!=0)&&((millis()-atime)>250))
+   if ((faderactive0!=0)&&((millis()-atime0)>FADER_TIMEOUT))
    {
          AbsMouse.release(MOUSE_LEFT);
-         faderactive=0;
+         mousedn=0;
+         faderactive0=0;
    }
 
-  
+   a1=analogRead(AN1);
+   if (abs(a1-preva1)>20)
+   {
+      //Serial.println(a1);
+      preva1=a1;
+      if (faderactive1==0) fp=a1; //this is a new move starting. Remember where we started off from
+      atime1=millis();
+      v=932+((1022-966)*2*(fp-a1)/1024);
+      AbsMouse.move(1361, v);
+      if (faderactive1==0) 
+      {
+            faderactive1=1;
+            if (mousedn!=0) {AbsMouse.release(MOUSE_LEFT); faderactive0=0; timea=0; timeb=0; timec=0;}
+            AbsMouse.press(MOUSE_LEFT);
+            mousedn=1;
+      }
+   }
+
+   if ((faderactive1!=0)&&((millis()-atime1)>FADER_TIMEOUT))
+   {
+         AbsMouse.release(MOUSE_LEFT);
+         mousedn=0;
+         faderactive1=0;
+   }
+
+  }
   
 
-  delay(10); 
+  delay(1); 
 
 } 
---
+++ switchcaps - s.scad
@@ -0,0 +1,38 @@
+numb=2;
+cap=["s","s"];
+
+   for (i=[0:numb-1])
+   {
+
+difference() {
+
+   union () {
+    translate ([i*15,0,-1.5+1.4]) cube ( [11.5,11.5,3], center = true ); //buttons below panel
+
+    translate ([i*15,0,-1.5+3]) cube ( [11.5,11.5,3], center = true ); //buttons above panel
+
+    translate ([i*15,6,-0.8]) cube ( [11.5,2,1.5], center = true ); //stop below
+
+    translate ([i*15,-6.5,-0.4]) cube ( [11.5,3,0.4], center = true ); //hinge
+
+    translate ([i*15,-7.6,-1]) cube ( [15,3,2], center = true ); //mount bar
+
+    translate([i*15+5, 3.75, 2.65]) linear_extrude(0.4) rotate([0,0,180]) text(cap[i],size=5.5);
+
+
+
+   //translate ([0,-8.5,0.7]) cube ( [49,4,1.4], center = true ); //buttons
+
+   //translate ([0,-6.5,-0.4]) cube ( [49,4,3], center = true ); //buttons
+
+
+   };
+
+
+
+   translate ([i*15,-7.6,-0.75]) cube ( [11,1.5,1.5], center = true ); //locating recess
+
+   };
+
+   
+};
